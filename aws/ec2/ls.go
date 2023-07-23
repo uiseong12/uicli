779472edacd9b851394d98fed8e92e536584d2e8
uiseong12 <test@nav.com>
2023-07-24 07:39:48 +0900

    test

--- aws/ec2/ls.go
+++ aws/ec2/ls.go
@@ -1,6 +1,9 @@
 package ec2
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ec2"
@@ -14,7 +17,7 @@ func Ls(*cobra.Command, []string) {
 			Region: aws.String("ap-northeast-2"),
 		},
 	)
-	param := &ec2.DescribeInstanceAttributeInput{
+	params := &ec2.DescribeInstancesInput{
 		Filters: []*ec2.Filter{
 			{
 				Name: aws.String("instance-state-name"),
@@ -24,4 +27,37 @@ func Ls(*cobra.Command, []string) {
 			},
 		},
 	}
+
+	resp, err := svc.DescribeInstances(params)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, res := range resp.Reservations {
+		for _, inst := range res.Instances {
+			fmt.Println(formatInstance(inst))
+		}
+	}
+}
+
+func formatInstance(inst *ec2.Instance) string {
+	output := []string{
+		*inst.PublicIpAddress,
+		*inst.InstanceId,
+		*inst.State.Name,
+		(*inst.LaunchTime).Format("2006-01-02 15:04:05"),
+		lookupTag(inst, "Name"),
+	}
+	return strings.Join(output[:], "\t")
+}
+
+func lookupTag(inst *ec2.Instance, key string) string {
+	var value string
+	for _, t := range inst.Tags {
+		if *t.Key == key {
+			value = *t.Value
+			break
+		}
+	}
+	return value
 }
